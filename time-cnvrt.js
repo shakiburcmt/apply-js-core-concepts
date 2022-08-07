0231d64a7c914fbc9861fdecffa83a254d1cc835
shakiburcmt <shakibur.cmt@gmail.com>
2022-08-07 12:49:15 +0600

    last

---
+++ time-cnvrt.js
@@ -0,0 +1,7 @@
+function hourToMinute(hour) {
+    const minutes = hour * 60;
+    return minutes;
+}
+
+let hour = hourToMinute(4);
+console.log(hour, "minutes");
--- while_reverse-factorial.js
+++ while_reverse-factorial.js
@@ -2,8 +2,10 @@ function factorial(number) {
     let i = number;
     let result = 1;
     while (i >= 1) {
+        console.log(i);
         result = result * i;
         i--;
+
     }
     return result;
 }
